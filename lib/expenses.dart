f12fb5200ffed5d4443c564a62154e474d012673
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-10-01 13:01:34 +0200

    add list of expenses

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,5 @@
+import 'package:expenses/expenses_list.dart';
+import 'package:expenses/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -9,13 +11,41 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registerExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.19,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Pam',
+      amount: 9.19,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: 'Virgin',
+      amount: 10.19,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: 'Italo',
+      amount: 119.19,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Text('chart'),
-          Text('list'),
+          const Text('chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registerExpenses),
+          )
         ],
       ),
     );
